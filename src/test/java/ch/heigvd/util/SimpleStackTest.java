e975771b97c24934480f4f5bfe4ab066e1188855
Jonatan Perret <perretjonatan1@gmail.com>
2024-11-21 13:34:04 +0100

    added tostring and toarray

--- src/test/java/ch/heigvd/util/SimpleStackTest.java
+++ src/test/java/ch/heigvd/util/SimpleStackTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class SimpleStackTest {
 
     private SimpleStack<Integer> stack;
@@ -53,4 +52,27 @@ class SimpleStackTest {
         assertEquals(2, it.next());
         assertEquals(1, it.next());
     }
+
+    @Test
+    void testToString() {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assertEquals("3 2 1 ", stack.toString());
+    }
+
+    @Test
+    void testToArray() {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        Object[] arr = stack.toArray();
+        assertEquals(3, arr.length);
+        assertEquals(3, arr[0]);
+        assertEquals(2, arr[1]);
+        assertEquals(1, arr[2]);
+    }
+
 }
